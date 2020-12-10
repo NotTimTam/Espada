e47113e5f5b28f92864479b7e02cccbd3f88222a
HaydenW <gamerwiechens35@outlook.com>
2020-12-10 16:48:12 -0700

    Added proper spacing

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const backTopButton = document.getElementById("bttB");
 backTopButton.addEventListener("mouseover", function(e) {
     backTopButton.innerText = "^ ^ ^ ^ ^ ^ ^ ^ ^";
 }, false);
+
+
 backTopButton.addEventListener("mouseout", function(e) {
     backTopButton.innerText = "Back to top";
 }, false);
