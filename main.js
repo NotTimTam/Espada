b128affc51852503f30306d27cf2a51d91ff92f7
Tim Phillips <timmyiscool8@gmail.com>
2020-12-14 18:25:42 -0700

    Added smooth transitions to buttons on services page.

--- main.js
+++ main.js
@@ -44,70 +44,35 @@ function apply(element) {
     window.location.href = `apply.html#${title}||${extradata}`;
 }
 
-function myFunctionC() {
-    document.getElementById("myDropdownC").classList.toggle("show");
-}
-
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbtnC')) {
-        var dropdowns = document.getElementsByClassName("dropdown-contentC");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
-
 function myFunctionS() {
-    document.getElementById("myDropdownS").classList.toggle("show");
-}
+    if (document.getElementById("myDropdownS").style.maxHeight == "0px") {
+        console.log("was closed")
+        document.getElementById("myDropdownS").style.maxHeight = "300px";
+        document.getElementById("myDropdownS").style.padding = "15px";
 
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbtnS')) {
-        var dropdowns = document.getElementsByClassName("dropdown-contentS");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
+    } else {
+        console.log("was open")
+        document.getElementById("myDropdownS").style.maxHeight = "0px";
+        document.getElementById("myDropdownS").style.padding = "0px";
     }
 }
 
 function myFunctionG() {
-    document.getElementById("myDropdownG").classList.toggle("show");
-}
-
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbtnG')) {
-        var dropdowns = document.getElementsByClassName("dropdown-contentG");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
+    if (document.getElementById("myDropdownG").style.maxHeight == "0px") {
+        document.getElementById("myDropdownG").style.maxHeight = "300px";
+        document.getElementById("myDropdownG").style.padding = "15px";
+    } else {
+        document.getElementById("myDropdownG").style.maxHeight = "0px";
+        document.getElementById("myDropdownG").style.padding = "0px";
     }
 }
 
 function myFunctionP() {
-    document.getElementById("myDropdownP").classList.toggle("show");
-}
-
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbtnP')) {
-        var dropdowns = document.getElementsByClassName("dropdown-contentP");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
+    if (document.getElementById("myDropdownP").style.maxHeight == "0px") {
+        document.getElementById("myDropdownP").style.maxHeight = "300px";
+        document.getElementById("myDropdownP").style.padding = "15px";
+    } else {
+        document.getElementById("myDropdownP").style.maxHeight = "0px";
+        document.getElementById("myDropdownP").style.padding = "0px";
     }
 }
